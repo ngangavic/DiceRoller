45be13027e771255865a55d1eae2ee8f554e7c35
ngangavic <ngangavictor10@gmail.com>
2019-10-05 17:11:25 +0300

    variables

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val diceImage = findViewById<Button>(R.id.button)
+        val btn_roll = findViewById<ImageView>(R.id.imageView)
     }
 }
